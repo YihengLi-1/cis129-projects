8b8535bc39a1ab1cf13f3b586b1f844ba291c621
YihengLi-1 <yli19@mail.pima.edu>
2024-09-22 18:09:39 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+#Author: student Yiheng Li
+#My first Python lab in my CIS129 course
+print("In a small house, a curious mouse named Nibbles peeked out.")
+print("He found a cheese-filled trap and couldn't resist taking a nibble.")
+print("Suddenly, the trap snapped, but Nibbles was quick and escaped!")
+print("From then on, he learned to be careful but never stopped exploring.")
